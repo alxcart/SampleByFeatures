655abb8b5cfa55be90efdfae67c4a5a75fff2b95
Alex <alxcart@gmail.com>
2019-07-07 10:10:01 -0300

    Files uploads
    version 0.2

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SampleByFeatures
-This plugin calculates the sample size (n) from population size (N) and inspection level (I, II or III).
+# samplebyfeatures
---
+++ constants.py
@@ -0,0 +1,5 @@
+#CONSTANTS
+from qgis.core import *
+# Sampling plan / Plano de amostragem
+#Inspection level / Nivel_de_Inspecao = self.dlg.comboBoxLevel.currentIndex()
+dicSampleLength={2:[2,2,3],9:[2,3,5],16:[3,5,8],26:[5,8,13],51:[5,13,20],91:[8,20,32],151:[13,32,50],281:[20,50,80],501:[32,80,125],1201:[50,125,200],3201:[80,200,315],10001:[125,315,500],35001:[200,500,800],150001:[315,800,1250],500001:[500,1250,2000]}
